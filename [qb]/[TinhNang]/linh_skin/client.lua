2e0f8515bba5f1e8294de9fea3ba458bc38341da
MityTin <43299672+MityTin@users.noreply.github.com>
2025-05-23 21:58:04 +0700

    Tinh NANG PED

--- [qb]/[TinhNang]/linh_skin/client.lua
+++ [qb]/[TinhNang]/linh_skin/client.lua
@@ -1,114 +1,128 @@
+local QBCore = exports['qb-core']:GetCoreObject()
 local skinlist = {}
-ESX	= nil
 local delay = 0
-Citizen.CreateThread(function()
-	while ESX == nil do
-		TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-		Citizen.Wait(0)
-	end
-	Citizen.Wait(10000)
-	ESX.TriggerServerCallback("linh_skin:getownedskin", function(skin)
-		skinlist = skin
-	end)
+
+-- Lấy danh sách skin sau 10 giây khi QBCore đã sẵn sàng
+CreateThread(function()
+    Wait(10000)
+    QBCore.Functions.TriggerCallback("linh_skin:getownedskin", function(skin)
+        skinlist = skin
+    end)
 end)
 
-RegisterNetEvent("linh_skin:update")
-AddEventHandler("linh_skin:update", function()
-	ESX.TriggerServerCallback("linh_skin:getownedskin", function(skin)
-		skinlist = skin
-	end)
+-- Khi có cập nhật skin từ server
+RegisterNetEvent("linh_skin:update", function()
+    QBCore.Functions.TriggerCallback("linh_skin:getownedskin", function(skin)
+        skinlist = skin
+    end)
 end)
 
-RegisterCommand("linh_skin",function()
-	ESX.UI.Menu.CloseAll()
-	local playerped = PlayerPedId()
-	local elements = {
-		{label = 'Skin mặc định', name = 'citizen_wear'}
-	}
-	if skinlist ~= {} then
-		local dumped = ESX.DumpTable(skinlist)
-		print(dumped)
-		print(skinlist.name)
-		for k,v in ipairs(skinlist) do
-			table.insert(elements, {
-				name = v.name,
-				label = v.name
-			})
-		end
-		
-		ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'vip_skin',{
-			title = 'Vip Skin',
-			align = 'top-left',
-			elements = elements
-		}, function (data,menu)
-		    if delay ~= 0 then
-		    	return ESX.ShowNotification('Đang bị delay còn ~r~'..delay)
-		    end
-			if data.current.name == 'citizen_wear' then
-				delay = 100
-				local curHea = GetEntityHealth(PlayerPedId())
-				print(curHea)
-				ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin, jobSkin)
-					local isMale = skin.sex == 0
+-- Lệnh mở menu skin
+RegisterCommand("linh_skin", function()
+    local playerPed = PlayerPedId()
+    local elements = {
+        {label = "Skin mặc định", name = "citizen_wear"}
+    }
 
-					TriggerEvent('skinchanger:loadDefaultModel', isMale, function()
-						ESX.TriggerServerCallback('esx_skin:getPlayerSkin', function(skin)
-							TriggerEvent('skinchanger:loadSkin', skin)
-							TriggerEvent('esx:restoreLoadout')
-						end)
-					end)
+    if next(skinlist) ~= nil then
+        for _, v in ipairs(skinlist) do
+            table.insert(elements, {
+                label = v.name,
+                name = v.name
+            })
+        end
 
-				end)
-				Citizen.Wait(1000)
-				SetEntityHealth(PlayerPedId(), curHea)
-				Citizen.Wait(10000)
-			else
-			ESX.UI.Menu.Open('default', GetCurrentResourceName(), 'vip_skin_confirm',
-			{
-				title = 'bạn muốn đổi thành skin này',
-				align = 'top-left',
-				elements = {
-					{ label = 'Không', value = 'no'},
-					{ label = 'Có', value = 'yes'}
-				}
-			}, function (data2, menu2)
-					if data2.current.value == 'yes' then
-						delay = 100
-						local curHea = GetEntityHealth(PlayerPedId())
-						characterModel = data.current.name
-						RequestModel(characterModel)
-						while not HasModelLoaded(characterModel) do
-							RequestModel(characterModel)
-							Citizen.Wait(0)
-						end
-						if IsModelInCdimage(characterModel) and IsModelValid(characterModel) then
-							SetPlayerModel(PlayerId(), characterModel)
-							SetPedDefaultComponentVariation(playerPed)
-							TriggerEvent('esx:restoreLoadout')
-							SetEntityHealth(PlayerPedId(), curHea)
-						end
-						SetModelAsNoLongerNeeded(characterModel)
-					else
-						menu2.close()
-					end
-			end, function (data2, menu2)
-				menu2.close()
-			end)
-		end
-		end, function (data,menu)
-			menu.close()
-		end, function (data, menu)
-		end)
-	else 
-		ESX.ShowNotification('Bạn ~r~ Không ~w~ có skin')
-	end
+        exports['qb-menu']:openMenu({
+            {
+                header = "VIP Skin",
+                isMenuHeader = true
+            },
+            table.unpack(
+                QBCore.Shared.TableToMenuItems(elements, function(item)
+                    return {
+                        header = item.label,
+                        txt = "Chọn skin này",
+                        params = {
+                            event = "linh_skin:selectSkin",
+                            args = { name = item.name }
+                        }
+                    }
+                end)
+            )
+        })
+    else
+        TriggerEvent("QBCore:Notify", "Bạn không có skin nào!", "error")
+    end
 end)
-				
-Citizen.CreateThread(function()
-	while true do
-		Citizen.Wait(1000)
-		if delay > 0 then
-			delay = delay - 1
-		end
-	end
+
+-- Khi chọn skin
+RegisterNetEvent("linh_skin:selectSkin", function(data)
+    local name = data.name
+    if delay ~= 0 then
+        return TriggerEvent("QBCore:Notify", "Đang bị delay còn ~r~" .. delay, "error")
+    end
+
+    local playerPed = PlayerPedId()
+    local curHea = GetEntityHealth(playerPed)
+
+    if name == "citizen_wear" then
+        delay = 100
+        TriggerServerEvent("qb-clothing:loadPlayerSkin")
+        Wait(1000)
+        SetEntityHealth(playerPed, curHea)
+        Wait(10000)
+    else
+        exports['qb-menu']:openMenu({
+            {
+                header = "Bạn muốn đổi thành skin này?",
+                isMenuHeader = true
+            },
+            {
+                header = "Có",
+                params = {
+                    event = "linh_skin:applySkin",
+                    args = { model = name, curHea = curHea }
+                }
+            },
+            {
+                header = "Không",
+                params = {
+                    event = ""
+                }
+            }
+        })
+    end
+end)
+
+-- Áp dụng model mới
+RegisterNetEvent("linh_skin:applySkin", function(data)
+    delay = 100
+    local playerPed = PlayerPedId()
+    local model = data.model
+    local curHea = data.curHea
+
+    RequestModel(model)
+    while not HasModelLoaded(model) do
+        Wait(0)
+    end
+
+    if IsModelInCdimage(model) and IsModelValid(model) then
+        SetPlayerModel(PlayerId(), model)
+        SetPedDefaultComponentVariation(PlayerPedId())
+        TriggerEvent('QBCore:Client:OnPlayerLoaded') -- Khôi phục vũ khí
+        SetEntityHealth(PlayerPedId(), curHea)
+        SetModelAsNoLongerNeeded(model)
+    else
+        TriggerEvent("QBCore:Notify", "Model không hợp lệ", "error")
+    end
+end)
+
+-- Delay reset
+CreateThread(function()
+    while true do
+        Wait(1000)
+        if delay > 0 then
+            delay = delay - 1
+        end
+    end
 end)
