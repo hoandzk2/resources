ca8d9f90e252ed1dcbfdcac6477a9bb24aba70cc
MityTin <43299672+MityTin@users.noreply.github.com>
2025-05-23 21:55:12 +0700

    TN

--- [qb]/[TinhNang]/linh_skin/server.lua
+++ [qb]/[TinhNang]/linh_skin/server.lua
@@ -1,30 +1,38 @@
-ESX              = nil
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+local QBCore = exports['qb-core']:GetCoreObject()
 
-ESX.RegisterServerCallback("linh_skin:getownedskin", function(source, cb)
-	local xPlayer = ESX.GetPlayerFromId(source)
-	local identifier = xPlayer.getIdentifier()
-	MySQL.Async.fetchAll('SELECT * FROM linh_skin WHERE identifier = @identifier', {['@identifier'] = identifier}, function(result)
-		cb(result)
-	end)
+-- Callback để lấy danh sách skin đã sở hữu
+QBCore.Functions.CreateCallback("linh_skin:getownedskin", function(source, cb)
+    local Player = QBCore.Functions.GetPlayer(source)
+    local identifier = Player.PlayerData.citizenid
+    MySQL.Async.fetchAll('SELECT * FROM linh_skin WHERE identifier = @identifier', {
+        ['@identifier'] = identifier
+    }, function(result)
+        cb(result)
+    end)
 end)
 
-TriggerEvent('es:addGroupCommand', 'addskin', 'superadmin', function(source, args, user)
-	if args[1] ~= nil then
-		if GetPlayerName(tonumber(args[1])) ~= nil then
-			if args[2] ~= nil then
-				addskin( tonumber(args[1]), (args[2]))
-			end
-		end
-	end
-	end,function(source, args, user)
-		TriggerClientEvent('chat:addMessage', source, { args = { '^1SYSTEM', 'Insufficient Permissions.' } })
-end,{help = ''})
+-- Lệnh dành cho admin thêm skin cho người chơi
+QBCore.Commands.Add("addskin", "Thêm skin cho người chơi (admin)", {{name="id", help="ID người chơi"}, {name="skinname", help="Tên skin"}}, true, function(source, args)
+    local targetId = tonumber(args[1])
+    local skinName = args[2]
+    
+    if GetPlayerName(targetId) then
+        AddSkin(targetId, skinName)
+    else
+        TriggerClientEvent('QBCore:Notify', source, "ID người chơi không hợp lệ.", "error")
+    end
+end, "admin") -- Hoặc "god" nếu bạn chỉ muốn cho người có quyền cao nhất
 
-function addskin(id, name)
-	local xPlayer = ESX.GetPlayerFromId(id)
-	local identifier = xPlayer.getIdentifier()
-	MySQL.Sync.execute("INSERT INTO linh_skin (identifier, name) VALUE (@identifier, @name)", {['@identifier'] = identifier, ['@name'] = name})
-	TriggerClientEvent('esx:showNotification', id, 'Bạn đã nhận được một skin')
-	TriggerClientEvent("linh_skin:update", id)
+-- Hàm thêm skin vào DB
+function AddSkin(id, name)
+    local Player = QBCore.Functions.GetPlayer(id)
+    local identifier = Player.PlayerData.citizenid
+
+    MySQL.Async.execute("INSERT INTO linh_skin (identifier, name) VALUES (@identifier, @name)", {
+        ['@identifier'] = identifier,
+        ['@name'] = name
+    }, function(rowsChanged)
+        TriggerClientEvent('QBCore:Notify', id, "Bạn đã nhận được một skin!", "success")
+        TriggerClientEvent("linh_skin:update", id)
+    end)
 end
