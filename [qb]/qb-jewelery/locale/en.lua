16bc6051c0edc1b1d99ae20c29474dc8ab846042
MityTin <43299672+MityTin@users.noreply.github.com>
2025-05-23 17:11:22 +0700

    VH

--- [qb]/qb-jewelery/locale/en.lua
+++ [qb]/qb-jewelery/locale/en.lua
@@ -1,20 +1,23 @@
 local Translations = {
     error = {
-        fingerprints = 'You\'ve left a fingerprint on the glass',
-        minimum_police = 'Minimum of %{value} police needed',
-        wrong_weapon = 'Your weapon is not strong enough..',
-        to_much = 'You have to much in your pocket'
+        fingerprints = 'Bạn đã để lại dấu vân tay trên tủ kính',
+        minimum_police = 'Cần ít nhất %{value} cảnh sát để thực hiện',
+        wrong_weapon = 'Vũ khí của bạn không đủ mạnh...',
+        to_much = 'Bạn đang mang quá nhiều đồ trong túi'
     },
-    success = {},
+    success = {
+        -- Nếu có thông báo thành công nào, bạn có thể thêm ở đây
+    },
     info = {
-        progressbar = 'Smashing the display case',
+        progressbar = 'Đang đập vỡ tủ trưng bày',
     },
     general = {
-        target_label = 'Smash the display case',
-        drawtextui_grab = '[E] Smash the display case',
-        drawtextui_broken = 'Display case is broken'
+        target_label = 'Đập vỡ tủ trưng bày',
+        drawtextui_grab = '[E] Đập vỡ tủ trưng bày',
+        drawtextui_broken = 'Tủ trưng bày đã bị vỡ'
     }
 }
+
 
 Lang = Lang or Locale:new({
     phrases = Translations,
