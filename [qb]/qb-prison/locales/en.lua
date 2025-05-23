8066318203ae8de6e8e661cc628f73a2bb9d0b97
MityTin <43299672+MityTin@users.noreply.github.com>
2025-05-23 17:47:44 +0700

    Tối Ưu VH

--- [qb]/qb-prison/locales/en.lua
+++ [qb]/qb-prison/locales/en.lua
@@ -1,42 +1,43 @@
 local Translations = {
     error = {
-        ["missing_something"] = "It looks like you are missing something...",
-        ["not_enough_police"] = "Not enough Police..",
-        ["door_open"] = "The door is already open..",
-        ["cancelled"] = "Process Canceled..",
-        ["didnt_work"] = "It did not work..",
-        ["emty_box"] = "The Box Is Empty..",
-        ["injail"] = "You're in jail for %{Time} months..",
-        ["item_missing"] = "You are missing an Item..",
-        ["escaped"] = "You escaped... Get the hell out of here.!",
-        ["do_some_work"] = "Do some work for sentence reduction, instant job: %{currentjob} ",
-        ["security_activated"] = "Highest security level is active, stay with the cell blocks!"
+        ["missing_something"] = "Có vẻ như bạn đang thiếu gì đó...",
+        ["not_enough_police"] = "Không đủ cảnh sát..",
+        ["door_open"] = "Cửa đã mở rồi..",
+        ["cancelled"] = "Đã hủy quá trình..",
+        ["didnt_work"] = "Không thành công..",
+        ["emty_box"] = "Chiếc hộp rỗng..",
+        ["injail"] = "Bạn bị giam %{Time} tháng..",
+        ["item_missing"] = "Bạn đang thiếu một vật phẩm..",
+        ["escaped"] = "Bạn đã trốn thoát... Mau chạy đi.!",
+        ["do_some_work"] = "Làm vài công việc để giảm án, công việc hiện tại: %{currentjob}",
+        ["security_activated"] = "Mức an ninh cao nhất đã được kích hoạt, hãy ở lại khu giam giữ!"
     },
     success = {
-        ["found_phone"] = "You found a phone..",
-        ["time_cut"] = "You've worked some time off your sentence.",
-        ["free_"] = "You're free! Enjoy it! :)",
-        ["timesup"] = "Your time is up! Check yourself out at the visitors center",
+        ["found_phone"] = "Bạn đã tìm thấy một chiếc điện thoại..",
+        ["time_cut"] = "Bạn đã làm việc để giảm án phạt.",
+        ["free_"] = "Bạn được tự do rồi! Hãy tận hưởng! :)",
+        ["timesup"] = "Đã đến lúc mãn hạn tù! Hãy đến quầy tiếp tân để ra trại"
     },
     info = {
-        ["timeleft"] = "You still have to... %{JAILTIME} months",
-        ["lost_job"] = "You're Unemployed",
-        ["job_interaction"] = "[E] Electricity Work",
-        ["job_interaction_target"] = "Do %{job} Work",
-        ["received_property"] = "You got your property back..",
-        ["seized_property"] = "Your property has been seized, you'll get everything back when your time is up..",
-        ["cells_blip"] = "Cells",
-        ["freedom_blip"] = "Jail Front Desk",
-        ["canteen_blip"] = "Canteen",
-        ["work_blip"] = "Prison Work",
-        ["target_freedom_option"] = "Check Time",
-        ["target_canteen_option"] = "Get Food",
-        ["police_alert_title"] = "New Call",
-        ["police_alert_description"] = "Prison Outbreak",
-        ["connecting_device"] = "Connecting Device",
-        ["working_electricity"] = "Connecting Wires"
+        ["timeleft"] = "Bạn vẫn còn... %{JAILTIME} tháng",
+        ["lost_job"] = "Bạn đã mất việc",
+        ["job_interaction"] = "[E] Sửa điện",
+        ["job_interaction_target"] = "Làm công việc %{job}",
+        ["received_property"] = "Bạn đã nhận lại tài sản của mình..",
+        ["seized_property"] = "Tài sản của bạn đã bị tịch thu, bạn sẽ nhận lại khi mãn hạn tù..",
+        ["cells_blip"] = "Phòng giam",
+        ["freedom_blip"] = "Quầy tiếp tân nhà tù",
+        ["canteen_blip"] = "Nhà ăn",
+        ["work_blip"] = "Khu lao động",
+        ["target_freedom_option"] = "Kiểm tra thời gian còn lại",
+        ["target_canteen_option"] = "Lấy đồ ăn",
+        ["police_alert_title"] = "Cuộc gọi mới",
+        ["police_alert_description"] = "Vượt ngục tại nhà tù",
+        ["connecting_device"] = "Đang kết nối thiết bị",
+        ["working_electricity"] = "Đang nối dây điện"
     }
 }
+
 
 Lang = Lang or Locale:new({
     phrases = Translations,
